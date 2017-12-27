b8837b6cdae1d24ff046d00d2cfc637f9879702b
Alex Greco <akgrecks88@gmail.com>
2017-12-26 21:17:57 -0500

    The app now renders red boxes based off the ctrl's box number.

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 (function(){
-
+    'use strict';
     var app = angular.module("myApp", []);
 
-    app.directive("thing", function(){
+    // Directive for displaying the IR-BOXES
+    app.directive("irBoxes", function(){
         return {
-            template: '<h1>Hello</h1>'
+            restrict: 'EA',
+            scope: {
+                numBoxes : '=boxes',
+                numArr: '=numarr'
+            },
+            templateUrl: 'ir-boxes.html'
         }
-    })
+    });
+
+    // Directive for quantity selector
+    app.directive('boxCounter', function(){
+       return {
+           templateUrl: 'box-counter.html',
+           scope: {
+               numBoxes : '=boxes'
+           }
+       }
+    });
+
+    // Controller to drive the directives
+    app.controller('irController', function($scope){
+        $scope.Boxes = 3;
+        $scope.NumArray = getNumArray($scope.Boxes);
+
+        function getNumArray(num){
+            var numArr = [];
+            for(var i = 1; i <= num; i++){
+                numArr.push(i);
+            }
+            return numArr;
+        }
+
+    });
+
+
 
 })();
